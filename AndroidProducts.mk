5aac87103552f0fccdcbfbfadd2984d739837bb1
fKm <fmetzner86@gmail.com>
2021-05-23 15:34:02 +0200

    instantnoodlep: Potatofy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_instantnoodlep.mk
+    $(LOCAL_DIR)/potato_instantnoodlep.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_instantnoodlep-user \
-    lineage_instantnoodlep-userdebug \
-    lineage_instantnoodlep-eng
+    potato_instantnoodlep-user \
+    potato_instantnoodlep-userdebug \
+    potato_instantnoodlep-eng
