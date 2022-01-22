c8ddca4ce5cc1b0df08e0be796436012496fd4ed
fKm <fmetzner86@gmail.com>
2022-01-22 10:27:57 +0100

    instantnoodlep: blissify

--- bliss_instantnoodlep.mk
+++ bliss_instantnoodlep.mk
@@ -21,10 +21,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from instantnoodlep device
 $(call inherit-product, device/oneplus/instantnoodlep/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_instantnoodlep
+# OnePlus Camera
+$(call inherit-product-if-exists, vendor/oneplus/apps/sm8250/config.mk)
+
+PRODUCT_NAME := bliss_instantnoodlep
 PRODUCT_DEVICE := instantnoodlep
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
