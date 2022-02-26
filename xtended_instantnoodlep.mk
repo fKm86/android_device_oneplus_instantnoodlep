f025064a2fe30e6e2bd0a59a993410c161be0a07
fKm <fmetzner86@gmail.com>
2022-02-26 11:03:51 +0100

    instanoodlep: xtend

--- xtended_instantnoodlep.mk
+++ xtended_instantnoodlep.mk
@@ -21,22 +21,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from instantnoodlep device
 $(call inherit-product, device/oneplus/instantnoodlep/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
+# Inherit Google Apps
+WITH_GAPPS := true
+TARGET_INCLUDE_EXTRA_GAPPS := true
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1440
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_instantnoodlep
+PRODUCT_NAME := xtended_instantnoodlep
 PRODUCT_DEVICE := instantnoodlep
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := IN2023
-CUSTOM_DEVICE := OnePlus8Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus8Pro \
     PRODUCT_NAME=OnePlus8Pro
+
+XTENDED_BUILD_TYPE := UNOFFICIAL
+XTENDED_BUILD_MAINTAINER := fKm
